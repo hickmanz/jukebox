342ecb1b9b0a52ad590fcd74d23bf8c240ba692d
Zack <hickmanzackary@gmail.com>
2017-06-21 17:08:57 -0500

    initial push

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var SpotifyWebApi = require('spotify-web-api-node');
+
+app.use('/', express.static('public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+    socket.on('search', function(data){
+        console.log('search term: ' + data);
+
+        spotifyApi.searchArtists(data)
+            .then(function(response) {
+                socket.emit('artistSrchResp', response);
+            }, function(err) {
+                console.error(err);
+            });
+        spotifyApi.searchAlbums(data)
+            .then(function(response2) {
+                socket.emit('albumSrchResp', response2);
+            }, function(err) {
+                console.error(err);
+            });
+        spotifyApi.searchTracks(data)
+            .then(function(response3) {
+                socket.emit('trackSrchResp', response3);
+            }, function(err) {
+                console.error(err);
+            });      
+
+    });
+
+});
+
+var authorizationCode = 'AQCjbfmpSDfUA8remT2ukDbVFAeVTWhln4aOwSObE8CLrcBPgLiWqif6Xi7qE7d5nBkhUzi_nZh2w20sO7MxtQrYpz0BF8FjxD0cA1mcjeaVhYzkXhFsEu17PldOS2rfFURDIGtGLXscg2vSpmuzTqrAoNXD6FQEKNV0xE6hoiQwy7qU-hmn1UIfIxMBMn0fZwCvL4UnS2sIcrnqaOj-H-wxHwq7BhuCg6EmeFdSvDvtI8YwAaLoF_DuXJ_SJHQh9HaKu_tLp7Zo9bAVWxHSsL_X5qoc84KvuV5tB_d2h0veayNqeEInftXjkpAcemYNbPwxGq6XXg';
+var spotifyApi = new SpotifyWebApi({
+  clientId : 'fd4b30f8ddd544ba8fcedd8d99e80e50',
+  clientSecret : 'c935d0a44207400d994571277722a095',
+  redirectUri : 'http://westworld:3000/'
+});
+
+clientCodeGrant();
+
+function clientCodeGrant(){
+    // Retrieve an access token
+    spotifyApi.clientCredentialsGrant()
+    .then(function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+    }, function(err) {
+        console.log('Something went wrong when retrieving an access token', err.message);
+    });
+}
+
+function authCodeGrant(){
+
+    var spotifyTokenExpirationEpoch;
+
+    spotifyApi.authorizationCodeGrant(authorizationCode)
+    .then(function(data) {
+
+        // Set the access token and refresh token
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+        // Save the amount of seconds until the access token expired
+        spotifyTokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+        console.log('Retrieved token. It expires in ' + Math.floor(spotifyTokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+    }, function(err) {
+        console.log('Something went wrong when retrieving the access token!', err.message);
+    });
+
+    var numberOfTimesUpdated = 0;
+
+    setInterval(function() {
+    console.log('Time left: ' + Math.floor((spotifyTokenExpirationEpoch - new Date().getTime() / 1000)) + ' seconds left!');
+
+    // OK, we need to refresh the token. Stop printing and refresh.
+    if (++numberOfTimesUpdated > 5) {
+        clearInterval(this);
+
+        // Refresh token and print the new time to expiration.
+        spotifyApi.refreshAccessToken()
+        .then(function(data) {
+            spotifyTokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+            console.log('Refreshed token. It now expires in ' + Math.floor(spotifyTokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+        }, function(err) {
+            console.log('Could not refresh the token!', err.message);
+        });
+    }
+    }, 1000);
+}
+
+
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ public/js/main.js
@@ -0,0 +1,112 @@
+var playlistShown = true;
+
+$(function () {
+
+    var socket = io();
+
+    $('form').submit(function(){
+      socket.emit('search', $('#query').val());
+      return false;
+    });
+    socket.on('artistSrchResp',function(data){
+        var i;
+        var artists = data.body.artists.items;
+        if(artists.length < 5){
+            i = artists.length;
+        } else {
+            i = 9;
+        }
+
+        for (var k=0; k < i; k++) {
+            var div = $("<div />");
+            div.html(getArtistDiv(artists[k]));
+            $("#artist-holder-inner").append(div);
+        }
+    });
+    socket.on('trackSrchResp',function(data){
+        var tracks = data.body.tracks.items;
+
+        var div = $("<li />");
+        div.html('<li class="track-row top-row"><div class="add"></div><div class="preview"></div><div class="title">TITLE</div><div class="artist">ARTIST</div><div class="album">ALBUM</div><div class="duration"><i class="icon-clock"></i></div><div class="popularity"><i class="icon-heart"></i></div></li>');
+        $("#track-list").append(div);
+
+        for (var i=0; i < tracks.length; i++) {
+            var li = $("<li />");
+            li.html(getTrackDiv(tracks[i]));
+            $("#track-list").append(li);
+        }
+        console.dir(data);
+    });
+    socket.on('albumSrchResp',function(data){
+        var i;
+        var albums = data.body.albums.items;
+        if(albums.length < 5){
+            i = albums.length;
+        } else {
+            i = 9;
+        }
+
+        for (var k=0; k < i; k++) {
+            var div = $("<div />");
+            div.html(getAlbumDiv(albums[k]));
+            $("#album-holder-inner").append(div);
+        }
+        console.dir(data);
+    });
+    $('#search').click(function () { // When arrow is clicked
+        showSearch();
+    });
+
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+});
+
+function showSearch() {
+    $(".menu-expand-holder").addClass("expand");
+    $(".current-song-holder").addClass("collapse");
+    playlistShown = false;
+}
+
+function getArtistDiv(data){
+    var i = data.images.length - 2;
+    var imageUrl
+    if (i < 0){
+        imageUrl = "";
+    } else {
+        imageUrl = data.images[i].url;
+    }
+    return `<div class="artist-holder"><div class="picture" style="background-image: url('` + imageUrl + `')"></div>` + data.name + `</div>`;
+}
+function getAlbumDiv(data){
+    var i = data.images.length - 2;
+    var imageUrl
+    var artists = "";
+    if (i < 0){
+        imageUrl = "";
+    } else {
+        imageUrl = data.images[i].url;
+    }
+    for (i=0; i < data.artists.length; i++){
+        if (i < data.artists.length - 1){
+            artists += data.artists[i].name + ", "; 
+        } else {
+            artists += data.artists[i].name
+        }
+    }
+    return `<div class="album-holder"><div class="picture" style="background-image: url('` + imageUrl + `')"></div><div class="album">` + data.name + `</div><div class="artist">` + artists + `</div></div>`;
+}
+function getTrackDiv(data){
+    var artists = "";
+    for (i=0; i < data.artists.length; i++){
+        if (i < data.artists.length - 1){
+            artists += data.artists[i].name + ", "; 
+        } else {
+            artists += data.artists[i].name
+        }
+    }
+    var min = Math.floor((data.duration_ms/1000/60) << 0);
+    var sec = Math.floor((data.duration_ms/1000) % 60);
+    var duration = min + ':' + sec;
+
+    return '<li class="track-row"><div class="add"><i class="icon-plus"></i></div><div class="preview"><i class="icon-headphones"></i></div><div class="title">' + data.name + '</div><div class="artist">' + artists + '</div><div class="album">' + data.album.name + '</div><div class="duration">' + duration + '</div><div class="popularity">' + data.popularity + '</div></li>';
+}
